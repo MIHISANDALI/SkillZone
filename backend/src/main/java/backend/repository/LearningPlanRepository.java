92be86cbb61de60fe27e27a5fc0e1d813b2f918d
Pabodha Devindi <pabodhadevindi2002@gmail.com>
2025-04-26 18:12:30 +0530

    Learning Plan Repository Created

--- backend/src/main/java/backend/repository/LearningPlanRepository.java
+++ backend/src/main/java/backend/repository/LearningPlanRepository.java
@@ -1,5 +1,9 @@
 package backend.repository;
 
-public class LearningPlanRepository {
-    
+import backend.model.LearningPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface LearningPlanRepository extends JpaRepository<LearningPlan, Long> {
+    List<LearningPlan> findByUserId(String userId);
 }
